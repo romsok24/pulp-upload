e4d83042a36312c784d5701ef5c4b1458182e0f2
Roman Sokalski <62810569+romsok24@users.noreply.github.com>
2021-11-16 08:37:39 +0100

    Update pulp_container_images_processing.sh

--- bin/pulp_container_images_processing.sh
+++ bin/pulp_container_images_processing.sh
@@ -4,16 +4,16 @@
 #               in order to make it available on the given Pulp container registry instnace
 # Usage:        ./pulp_container_images_processing.sh
 # Version:      21v1030
-# Author:       roman.sokalski@hitachienergy.com
+# Author:       roman.sokalski
 
  
-CUST_BASE_PATH='/var/www/html/epirepo/'
+CUST_BASE_PATH='/var/www/html/rosrepo/'
 CUST_PATH=$CUST_BASE_PATH'images/*'
 ARTIFACT_FILTER='*'
-EPI_RELEASE='13'
-REPO_NAME='epi_reg_'$EPI_RELEASE
-EPI_REG_NAME='epi_reg_'$(cat  /etc/machine-id)
-EPI_REG_URL='http://51.145.162.94:5000'
+ros_RELEASE='13'
+REPO_NAME='ros_reg_'$ros_RELEASE
+ros_REG_NAME='ros_reg_'$(cat  /etc/machine-id)
+ros_REG_URL='http://51.145.162.94:5000'
 
 echo -e "\n=====Prereqs=======================================================\n"
 if [[ $# -lt 2 ]] ; then
@@ -30,23 +30,23 @@ source */bin/activate
 echo -e "\n=====Creating pulp repo and uploding artifacts=====================\n"
 pulp config create --username admin --overwrite --base-url http://$1 --password $2
 
-EPI_REPOS=$(curl -sk -X GET $EPI_REG_URL'/v2/_catalog' | jq '.repositories[]' | tr -d '"')
-EPI_REPOS_CNT=$(echo "$EPI_REPOS" | wc -l)
+ROS_REPOS=$(curl -sk -X GET $ros_REG_URL'/v2/_catalog' | jq '.repositories[]' | tr -d '"')
+ROS_REPOS_CNT=$(echo "$ROS_REPOS" | wc -l)
 c=1
-for r in $EPI_REPOS; do
+for r in $ROS_REPOS; do
     CONT_REPO_NAME=$(pulp container repository create --name $r | jq -r '.name')
 
-    r_TAG=$(curl -sk -X GET $EPI_REG_URL'/v2/'$r'/tags/list' | jq '.tags[]' | tr -d '"')
+    r_TAG=$(curl -sk -X GET $ros_REG_URL'/v2/'$r'/tags/list' | jq '.tags[]' | tr -d '"')
     printf "[INF] Processing %s:%s container image\n" "$r" "$r_TAG"
     
-    echo $(pulp container remote create --name $r --url $EPI_REG_URL --upstream-name $r --tls-validation false | jq -r '.pulp_created')
+    echo $(pulp container remote create --name $r --url $ros_REG_URL --upstream-name $r --tls-validation false | jq -r '.pulp_created')
     TASK_HREF=$(pulp container repository sync --name $CONT_REPO_NAME --remote $r | jq -r '.task')
     
     # REG_PATH=$(pulp container distribution create --name $REPO_NAME'_'$(date +"%m%d.%H%M") --base-path $r --repository $CONT_REPO_NAME  | jq -r '.registry_path')
     REG_PATH=$(pulp container distribution create --name $r --base-path $r --repository $CONT_REPO_NAME  | jq -r '.registry_path')
     printf "[INF] Image available at:  %s:%s \n" "$REG_PATH" "$r_TAG"
 
-    let PROGRESS=(c*100)/$EPI_REPOS_CNT
+    let PROGRESS=(c*100)/$ROS_REPOS_CNT
     printf "====== Progress:  %.0f %% =======================================\n" "$PROGRESS"
     c=$((c+1))
 done
